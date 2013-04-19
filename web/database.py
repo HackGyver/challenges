ff5f7c60e0669c59fd1e180eae1653185a0a6a6a
Tao Sauvage <sauvage.tao@gmail.com>
2013-04-19 23:00:11 +0200

    new models for challenge and category

--- web/database.py
+++ web/database.py
@@ -9,6 +9,13 @@
 from web import webdb as db
 
 
+# Many-to-many relationship between Category and Challenge
+categories = db.Table('categories',
+        db.Column('category_id', db.Integer, db.ForeignKey('category.id')),
+        db.Column('challenge_id', db.Integer, db.ForeignKey('challenge.id'))
+)
+
+
 class User(db.Model):
     __tablename__= 'users'
 
@@ -28,8 +35,36 @@ class User(db.Model):
 
     def get_id(self):
         return self.id
+
     def __repr__(self):
         return "<User('%s')>" % (self.username)
+
+
+class Challenge(db.Model):
+    __tablename__ = 'challenges'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.Text)
+    level = db.Column(db.Integer, nullable=False)
+    flag = db.Column(db.String(256), nullable=False)
+    url = db.Column(db.String(256), nullable=False)
+
+    categories = db.relationship('Category', secondary=categories,
+            backref=db.backref('challenges', lazy='dynamic'))
+
+    def __repr__(self):
+        return "<Challenge('%s')>" % (self.title)
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+    def __repr__(self):
+        return "<Category('%s')>" % (self.name)
 
 
 # Creation of the database
