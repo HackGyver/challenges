b7c8497cab306ed193c927af9d8245add907f96f
Tao Sauvage <sauvage.tao@gmail.com>
2013-04-19 15:40:48 +0200

    basic init file for configurations

---
+++ web/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+
+    Global definitions for the application 'web'.
+    'web' defines the web portal for the members.
+
+"""
+
+
+from flask import Flask
+from flask.ext import login
+from flask.ext.sqlalchemy import SQLAlchemy
+
+
+# Flask init
+webapp = Flask(__name__)
+webapp.config.from_pyfile('config.cfg')
+# Flask-SQLAlchemy init
+webdb = SQLAlchemy(webapp)
+
+
+# Import your definition files here
+# For instance:
+#   import web.database
+# It will import web.database which contains the definition models of the
+# database of the portal
+import web.main
+import web.database
+import web.login
